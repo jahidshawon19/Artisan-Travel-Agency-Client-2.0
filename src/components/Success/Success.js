c15081d7fad11f1c65b3ae3d36db0ca1ec96f565
jahidshawon19 <jahidshawon1730@gmail.com>
2022-09-20 10:06:22 +0600

    sslcommerz integrated

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer/Footer';
 import Navbar from '../Shared/Navbar/Navbar';
 
 const Success = () => {
@@ -6,14 +7,19 @@ const Success = () => {
         <>
             <Navbar></Navbar>
             <div className="container">
+         
                 <div className="row py-5">
                     <div className="col-lg-2"></div>
                     <div className="col-lg-8 mt-5">
-                        <img src="https://www.beauty-addict.com/wp-content/uploads/2021/02/Payment-success.png" alt="" className='img-fluid' />
+                    <div class="alert alert-primary mt-5" role="alert">
+                        Your invoice is ready. Check out your profile
+                    </div>
+                        <img src="https://www.beauty-addict.com/wp-content/uploads/2021/02/Payment-success.png" alt="" className='img-fluid' style={{'height':'60%'}} />
                     </div>
                     <div className="col-lg-2"></div>
                 </div>
             </div>
+            <Footer></Footer>
         </>
     );
 };
